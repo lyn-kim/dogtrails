df814e138300c7077240f359ee8d304535aa429b
Lyn Kim <lynkim216@outlook.com>
2022-03-29 14:59:07 -0700

    create separate trail functional component and import into search-list and all-list components to render complete list

---
+++ client/components/trail.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function Trail(props) {
+  const { trailName, length, location, photoUrl, difficulty } = props.trail;
+
+  return (
+    <>
+    <div className="row trail-entry">
+      <div className="column-three-fifth position-relative">
+        <div className="row">
+          <div className="column-three-fourth trail-name">
+            <p className="trail-name">{trailName}</p>
+          </div>
+        </div>
+        <div className="row align-content-center">
+          <div className="row display-block text-align-center">
+            <span className="trail-length-num">{length}</span><span className="trail-length-mi">miles</span>
+            <div className="row">
+              {
+                difficulty === 'easy'
+                  ? <p className="intensity-rating-easy">EASY</p>
+                  : difficulty === 'moderate'
+                    ? <p className="intensity-rating-moderate">MODERATE</p>
+                    : <p className="intensity-rating-difficult">DIFFICULT</p>
+              }
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <p className="position-absolute trail-address">{location}</p>
+        </div>
+      </div>
+      <div className="column-two-fifth">
+        <img className="trail-img" src={photoUrl} alt="image of trail" />
+      </div>
+    </div>
+    </>
+  );
+}
