841609566eb636fd0c1b9288819ad1c48b66dd7d
Lyn Kim <lynkim216@outlook.com>
2022-03-29 18:26:33 -0700

    update html on trail component and css to show trail image on ipad and mobile view

--- client/components/trail.jsx
+++ client/components/trail.jsx
@@ -5,40 +5,30 @@ export default function Trail(props) {
   const { trailId, trailName, length, location, photoUrl, difficulty } = props.trail;
 
   return (
-    <>
-    <div className="row trail-entry">
-      <div className="column-three-fifth position-relative">
-        <div className="row">
-          <div className="column-three-fourth trail-name">
-            <p className="trail-name">{trailName}</p>
+    <article className="trail-entry">
+      <img className="trail-img" src={photoUrl} alt="image of trail" />
+      <div className="column-three-fifth trail-entry-description">
+        <div className="row column-three-fourth trail-name trail-description-element">{trailName}</div>
+        {isDeleteButtonVisible && (
+          <div className="column-fourth trail-name text-align-end">
+            {
+              <a onClick={() => props.onOpenDeleteModal(trailId)}>
+                <i className="trash-icon fas fa-trash icon-margin"></i>
+              </a>
+            }
           </div>
-          { isDeleteButtonVisible && (
-            <div className="column-fourth trail-name text-align-end">
-              {
-                <a onClick={() => props.onOpenDeleteModal(trailId)}>
-                  <i className="trash-icon fas fa-trash icon-margin"></i>
-                </a>
-              }
-            </div>
-          )
-          }
-        </div>
-        <div className="row align-content-center">
-          <div className="row display-block text-align-center">
-            <span className="trail-length-num">{length}</span><span className="trail-length-mi">miles</span>
-            <div className={`row pill pill-${difficulty}`}>
+        )}
+        <div className="row display-block trail-description-element">
+          <span className="trail-length-num">{length}</span>
+          <span className="trail-length-mi">miles</span>
+          <div>
+            <div className={`pill pill-${difficulty}`}>
               { difficulty.toUpperCase() }
             </div>
           </div>
         </div>
-        <div className="row">
-          <p className="position-absolute trail-address">{location}</p>
-        </div>
+        <div className="trail-address">{location}</div>
       </div>
-      <div className="column-two-fifth">
-        <img className="trail-img" src={photoUrl} alt="image of trail" />
-      </div>
-    </div>
-    </>
+    </article>
   );
 }
