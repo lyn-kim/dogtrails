daead042baf5cfd48ce4876b127f63101288100b
Lyn Kim <lynkim216@outlook.com>
2022-05-03 16:46:23 -0700

    update Header component to functional component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,35 +1,89 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+// export default class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
 
-  render() {
-    return (
-        <div className="navbar">
-          <div className="row space-between">
-            <div>
-              <span className="display-flex"><i className="fas fa-mountain logo"></i>
-              <a href="#" className="logo-font"> DogTrails</a>
-              </span>
-            </div>
-            <ul className="nav-list">
-              <li className="nav-links"><a href="#all-list">EXPLORE</a></li>
-              <li className="nav-links">
-                {
-                  <a onClick={
-                    this.props.user === null
-                      ? () => this.props.onOpenAuthModal()
-                      : !this.props.menuOpen ? () => this.props.onShowMenu() : () => this.props.onHideMenu()
-                  }><i className="user-icon fas fa-user"></i></a>
-                }
-              </li>
-            </ul>
-          </div>
+//   render() {
+//     return (
+//         <div className="navbar">
+//           <div className="row space-between">
+//             <div>
+//               <span className="display-flex"><i className="fas fa-mountain logo"></i>
+//               <a href="#" className="logo-font"> DogTrails</a>
+//               </span>
+//             </div>
+//             <ul className="nav-list">
+//               <li className="nav-links"><a href="#all-list">EXPLORE</a></li>
+//               <li className="nav-links">
+//                 {
+//                   <a onClick={
+//                     this.props.user === null
+//                       ? () => this.props.onOpenAuthModal()
+//                       : !this.props.menuOpen ? () => this.props.onShowMenu() : () => this.props.onHideMenu()
+//                   }><i className="user-icon fas fa-user"></i></a>
+//                 }
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//     );
+//   }
+// }
+
+export default function Header(props) {
+
+  return (
+    <div className="navbar">
+      <div className="row space-between">
+        <div>
+          <span className="display-flex"><i className="fas fa-mountain logo"></i>
+            <a href="#" className="logo-font"> DogTrails</a>
+          </span>
         </div>
-    );
-  }
+        <ul className="nav-list">
+          <li className="nav-links"><a href="#all-list">EXPLORE</a></li>
+          <li className="nav-links">
+            {
+              <a onClick={
+                props.user === null
+                  ? () => props.onOpenAuthModal()
+                  : !props.menuOpen ? () => props.onShowMenu() : () => props.onHideMenu()
+              }><i className="user-icon fas fa-user"></i></a>
+            }
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+// export default function Header(props) {
+
+//   return (
+//     <div className="navbar">
+//       <div className="row space-between">
+//         <div>
+//           <span className="display-flex"><i className="fas fa-mountain logo"></i>
+//             <a href="#" className="logo-font"> DogTrails</a>
+//           </span>
+//         </div>
+//         <ul className="nav-list">
+//           <li className="nav-links"><a href="#all-list">EXPLORE</a></li>
+//           <li className="nav-links">
+//             {
+//               <a onClick={
+//                 this.props.user === null
+//                   ? () => this.props.onOpenAuthModal()
+//                   : !this.props.menuOpen ? () => this.props.onShowMenu() : () => this.props.onHideMenu()
+//               }><i className="user-icon fas fa-user"></i></a>
+//             }
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
