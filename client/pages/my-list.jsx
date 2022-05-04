dea992b20e2b3db0add3d5b25b2739be48bb67e5
Lyn Kim <lynkim216@outlook.com>
2022-05-04 15:51:45 -0700

    refactor my-list component with hooks

--- client/pages/my-list.jsx
+++ client/pages/my-list.jsx
@@ -1,63 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LoadingIndicator from '../components/loading-indicator';
 import NetworkError from './network-error';
 import Trail from '../components/trail';
-export default class MyList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      trails: [],
-      fetchInProgress: false,
-      networkError: false
-    };
-  }
 
-  componentDidMount() {
-    fetch('/api/my-trails', {
-      method: 'GET',
-      headers: {
-        'X-Access-Token': window.localStorage.getItem('react-context-jwt')
-      }
-    })
-      .then(this.setState({ fetchInProgress: true }))
-      .then(res => res.json())
-      .then(trails => {
-        this.setState({
-          trails: trails,
-          fetchInProgress: false
-        });
-      }
-      )
-      .catch(err => {
-        this.setState({ networkError: true });
-        console.error(err);
+export default function MyList(props) {
+
+  const [trails, setTrailList] = useState([]);
+  const [fetchInProgress, setFetchInProgress] = useState(false);
+  const [networkError, setNetworkErrorStatus] = useState(false);
+
+  useEffect(() => {
+    fetchTrails();
+  }, []);
+
+  const fetchTrails = async () => {
+    setFetchInProgress(true);
+    try {
+      const response = await fetch('/api/my-trails', {
+        method: 'GET',
+        headers: {
+          'X-Access-Token': window.localStorage.getItem('react-context-jwt')
+        }
       });
+      const trails = await response.json();
+      setTrailList(trails);
+      setFetchInProgress(false);
+    } catch (err) {
+      setNetworkErrorStatus(true);
+      console.error(err);
+    }
+  };
+
+  if (networkError) {
+    return <NetworkError />;
   }
 
-  render() {
-    if (this.state.networkError) {
-      return <NetworkError />;
-    }
-    if (this.state.fetchInProgress) {
-      return <LoadingIndicator />;
-    }
-    const notDeletedTrails = this.state.trails.filter(trail => !trail.isDeleted).length;
-    return (
-      <>
-        <div className="row justify-center" >
-          <h3 className="add-trail-title">My Trails</h3>
-        </div >
-        <div>
-          { notDeletedTrails === 0
-            ? <p className="no-trail-msg">Woof! No trails were found :-( </p>
-            : this.state.trails.filter(trail => !trail.isDeleted).map(trail => {
-              return (
-                <Trail isDeleteButtonVisible key={trail.trailId} trail={trail} onOpenDeleteModal={this.props.onOpenDeleteModal} />
-              );
-            })
-          }
-        </div>
-      </>
-    );
+  if (fetchInProgress) {
+    return <LoadingIndicator />;
   }
+
+  const notDeletedTrails = trails.filter(trail => !trail.isDeleted).length;
+
+  return (
+    <>
+      <div className="row justify-center" >
+        <h3 className="add-trail-title">My Trails</h3>
+      </div >
+      <div>
+        { notDeletedTrails === 0
+          ? <p className="no-trail-msg">Woof! No trails were found :-( </p>
+          : trails.filter(trail => !trail.isDeleted).map(trail => {
+            return (
+              <Trail isDeleteButtonVisible key={trail.trailId} trail={trail} onOpenDeleteModal={props.onOpenDeleteModal} />
+            );
+          })
+        }
+      </div>
+    </>
+  );
 }
